65e6463067ce0a567c8129285750b12fa2709121
Rick Jonkman <rickjonkman@MacBook-Air-van-Rick.local>
2024-06-05 15:37:50 +0200

    starting-point

---
+++ src/main/java/dev/rick/gymtracker2024/GymTracker2024Application.java
@@ -0,0 +1,13 @@
+package dev.rick.gymtracker2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymTracker2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymTracker2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/dev/rick/gymtracker2024/GymTracker2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rick.gymtracker2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymTracker2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
